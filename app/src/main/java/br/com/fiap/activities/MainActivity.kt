cad50dda57e017c7161dec67d265933033edee6d
DougGoncalves <doug.goncalvesf@gmail.com>
2020-12-28 11:46:34 -0300

    Add Splash Screen

--- app/src/main/java/br/com/fiap/activities/MainActivity.kt
+++ app/src/main/java/br/com/fiap/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.fiap
+package br.com.fiap.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fiap.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
